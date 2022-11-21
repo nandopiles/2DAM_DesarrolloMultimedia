fbcc54c61623e865b4534b22af3945cff34b31a8
Nando PL <118541862+nandopiles@users.noreply.github.com>
2022-11-21 13:45:37 +0100

    examen 1ª evaluación

---
+++ Examen45/app/src/main/java/dam/curso2022/examen45/Calculadora.java
@@ -0,0 +1,13 @@
+package dam.curso2022.examen45;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calculadora extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculadora);
+    }
+}
