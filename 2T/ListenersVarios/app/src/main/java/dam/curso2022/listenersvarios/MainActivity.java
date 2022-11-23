226f65eb94dc2c57054e8cb7e42323336a485cc7
Nando PL <118541862+nandopiles@users.noreply.github.com>
2022-11-23 09:53:20 +0100

    ejercicio Listeners varios

---
+++ 2T/ListenersVarios/app/src/main/java/dam/curso2022/listenersvarios/MainActivity.java
@@ -0,0 +1,31 @@
+package dam.curso2022.listenersvarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private Button btn1;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.tv1);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
