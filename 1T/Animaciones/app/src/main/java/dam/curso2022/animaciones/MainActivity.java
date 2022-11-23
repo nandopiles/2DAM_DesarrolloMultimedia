c550a87b05364ccc494313b404a2fc8438466dae
Nando PL <118541862+nandopiles@users.noreply.github.com>
2022-11-23 09:09:49 +0100

    actualización examen

--- 1T/Animaciones/app/src/main/java/dam/curso2022/animaciones/MainActivity.java
+++ 1T/Animaciones/app/src/main/java/dam/curso2022/animaciones/MainActivity.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Animation miAnimacion = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animacion);
+                miAnimacion.setRepeatMode(Animation.RESTART);
+                miAnimacion.setRepeatCount(2);
+                tv.startAnimation(miAnimacion);
             }
         });
     }
