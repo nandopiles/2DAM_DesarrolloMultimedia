9a303378ee3ff919a9728bed71f7d760e00a28a5
Nando PL <nandopiless@gmail.com>
2022-12-05 17:43:23 +0100

    actualización examen

--- 1T/Examen45/app/src/main/java/dam/curso2022/examen45/MainActivity.java
+++ 1T/Examen45/app/src/main/java/dam/curso2022/examen45/MainActivity.java
@@ -12,27 +12,23 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
 
     private TextView miColor;
-    private SeekBar sbRed;
-    private SeekBar sbGreen;
-    private SeekBar sbBlue;
+    private SeekBar sbRed, sbGreen, sbBlue;
     private Button miBoton;
-    private int red = 0;
-    private int green = 0;
-    private int blue = 0;
+    private int red = 0, green = 0, blue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        miColor = (TextView) findViewById(R.id.miColor);
-        sbRed = (SeekBar) findViewById(R.id.sbRed);
-        sbGreen = (SeekBar) findViewById(R.id.sbGreen);
-        sbBlue = (SeekBar) findViewById(R.id.sbBlue);
-        miBoton = (Button) findViewById(R.id.miBoton);
+        miColor = findViewById(R.id.miColor);
+        sbRed = findViewById(R.id.sbRed);
+        sbGreen = findViewById(R.id.sbGreen);
+        sbBlue = findViewById(R.id.sbBlue);
+        miBoton = findViewById(R.id.miBoton);
 
         /* No me crea correctamente el directorio fonts por lo que me pega petardazo
 
@@ -41,60 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
         miColor.setTextColor(Color.rgb(red, green, blue));
 
-        sbRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                red = i;
-                miColor.setText("Rojo=\"" + red + "\"" + " Green=\"" + green + "\"" + " Blue=\"" + blue + "\"");
-                miColor.setTextColor(Color.rgb(red, green, blue));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-        sbGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                green = i;
-                miColor.setText("Rojo=\"" + red + "\"" + " Green=\"" + green + "\"" + " Blue=\"" + blue + "\"");
-                miColor.setTextColor(Color.rgb(red, green, blue));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-        sbBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                blue = i;
-                miColor.setText("Rojo=\"" + red + "\"" + " Green=\"" + green + "\"" + " Blue=\"" + blue + "\"");
-                miColor.setTextColor(Color.rgb(red, green, blue));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+        sbRed.setOnSeekBarChangeListener(this);
+        sbBlue.setOnSeekBarChangeListener(this);
+        sbGreen.setOnSeekBarChangeListener(this);
 
         miBoton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,5 +48,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        switch (seekBar.getId()) {
+            case R.id.sbRed:
+                red = i;
+                break;
+            case R.id.sbGreen:
+                green = i;
+                break;
+            case R.id.sbBlue:
+                blue = i;
+                break;
+        }
+        miColor.setText("Rojo=\"" + red + "\"" + " Green=\"" + green + "\"" + " Blue=\"" + blue + "\"");
+        miColor.setTextColor(Color.rgb(red, green, blue));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
